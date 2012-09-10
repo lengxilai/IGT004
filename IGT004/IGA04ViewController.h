96fc9f60c0ae4c4ffed2da83c3a4513216dce0ba
wangchong0114 <wangchong0114@vip.qq.com>
2012-09-10 17:54:17 +0800

    111

---
+++ IGT004/IGA04ViewController.h
@@ -0,0 +1,13 @@
+//
+//  IGA04ViewController.h
+//  IGT004
+//
+//  Created by wang chong on 12-9-10.
+//  Copyright (c) 2012年 ntt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IGA04ViewController : UITableViewController
+
+@end
