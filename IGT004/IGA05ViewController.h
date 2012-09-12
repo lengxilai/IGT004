4a3f322e575035fc43c0121ce0788fb701511910
wangchong0114 <wangchong0114@vip.qq.com>
2012-09-12 15:54:43 +0800

    111

---
+++ IGT004/IGA05ViewController.h
@@ -0,0 +1,13 @@
+//
+//  IGA05ViewController.h
+//  IGT004
+//
+//  Created by wang chong on 12-9-12.
+//  Copyright (c) 2012年 ntt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IGA05ViewController : UIViewController
+
+@end
---
+++ IGT004/IGA05ViewController.m
@@ -0,0 +1,43 @@
+//
+//  IGA05ViewController.m
+//  IGT004
+//
+//  Created by wang chong on 12-9-12.
+//  Copyright (c) 2012年 ntt. All rights reserved.
+//
+
+#import "IGA05ViewController.h"
+
+@interface IGA05ViewController ()
+
+@end
+
+@implementation IGA05ViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
