9b12373263ef5e2784d99ab1a22795f022a6be92
wangchong0114 <wangchong0114@vip.qq.com>
2012-09-19 16:02:40 +0800

    111

--- IGT004/IGA01ViewController.m
+++ IGT004/IGA01ViewController.m
@@ -338,8 +338,8 @@
     [self.navigationController pushViewController:a02ViewController animated:YES];
 }
 -(void)showAlertWithPlacemark:(CLPlacemark *)placemark{
-    NSString *currentCity=[[placemark.addressDictionary objectForKey:@"City"] substringToIndex:3];
-    if(![currentCity isEqual:@"Dal" ]){
+    NSString *currentCity=[[placemark.addressDictionary objectForKey:@"City"] substringToIndex:2];
+    if(![currentCity isEqual:@"Dal" ] && ![currentCity isEqual:@"大连"] && ![currentCity isEqual:@"大連"]){
         UIAlertView *av = [[UIAlertView alloc]initWithTitle:@"" message:@"亲！你好像不在大连啊！要不你用列表来看吧？" delegate:self cancelButtonTitle:@"取消" otherButtonTitles:@"OK", nil ];
         [av show];
     }
